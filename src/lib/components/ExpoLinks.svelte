38ef2411b0f6fafb81f91f4ba3eb7a58d8f85905
Phillip Malboeuf <phil@phils.computer>
2022-07-19 10:10:19 -0400

    expo links fix

--- src/lib/components/ExpoLinks.svelte
+++ src/lib/components/ExpoLinks.svelte
@@ -33,7 +33,7 @@
       }
     }
 
-    &.active {
+    &.retour {
 
       &:after {
         content: " ×";
@@ -50,14 +50,14 @@
     <path d="M2.5 1C1.67157 1 1 1.67157 1 2.5V14.5001C1 15.3285 1.67157 16.0001 2.5 16.0001H18.3889C19.2173 16.0001 19.8889 15.3285 19.8889 14.5001V4.83342C19.8889 4.00499 19.2173 3.33342 18.3889 3.33342H12.1818V2.5C12.1818 1.67157 11.5102 1 10.6818 1H2.5Z" fill="none" stroke="currentColor" stroke-linejoin="round"/>
     </svg>
   </a>
-  <a class:active={type === 'slider'} href="/expositions/{expo.id}/oeuvres?type=slider">
+  <a class:active={type === 'slider'} class:retour={type === 'slider'} href={type === 'slider' ? `/expositions#${expo.id}` : `/expositions/${expo.id}/oeuvres?type=slider`}>
     <svg width="28" height="18" viewBox="0 0 28 18" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect x="1" y="1" width="7.3" height="8" fill="none" stroke="currentColor"/>
     <rect x="8.2998" y="1" width="11.5441" height="15.62" fill="none" stroke="currentColor"/>
     <rect x="19.8442" y="1" width="7.3" height="8" fill="none" stroke="currentColor"/>
     </svg>
   </a>
-  <a class:active={type === 'mur'} href="/expositions/{expo.id}/oeuvres?type=mur">
+  <a class:active={type === 'mur'} class:retour={type === 'mur'} href={type === 'mur' ? `/expositions#${expo.id}` : `/expositions/${expo.id}/oeuvres?type=mur`}>
     <svg width="18" height="18" viewBox="0 0 18 18" fill="none">
     <rect width="14.6218" height="14.6218" transform="translate(1.5 1.5)" fill="none"/>
     <rect x="1" y="1" width="7.81092" height="7.81092" stroke="currentColor"/>
