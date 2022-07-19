f08c7d6a121a5ddab2310f3e3ebee513a2846080
Phillip Malboeuf <phil@phils.computer>
2022-07-19 10:27:25 -0400

    Oeuvre

--- src/routes/expositions/[id]/oeuvres/[id].svelte
+++ src/routes/expositions/[id]/oeuvres/[id].svelte
@@ -64,6 +64,7 @@
 <script lang="ts">
   import { DateTime } from "luxon"
   import { onMount } from 'svelte'
+  import { page as p } from '$app/stores'
   import type { OeuvreDocument } from '$lib/components/Oeuvres.svelte'
   import type { SoumissionDocument } from 'src/routes/forms/[form_id]/soumissions/[id].svelte'
 
@@ -74,18 +75,13 @@
 
 	export let page: PageDocument
   export let oeuvre: SoumissionDocument
+
+  console.log($p.params)
 </script>
 
 <Page {page} />
 
-<article class="padded">
-  <h1 class="">{oeuvre.titre}</h1>
-
-  <p>
-    {#if oeuvre.nom}{oeuvre.nom}<br>{/if}
-    {#if oeuvre.ville}{oeuvre.ville}<br>{/if}
-    {#if oeuvre.date}{oeuvre.date}<br>{:else}Non datée<br>{/if}
-  </p>
+<article class="padded grid grid--halves">
 <!-- 
   <div class="grid">
     <span>{d?.toFormat('yyyy.mm.dd')} {f ? f.toFormat('yyyy.mm.dd') : ''}</span>
@@ -93,13 +89,24 @@
     <span>{readingTime} min</span>
     <span><a href="/expositions/{exposition.id}/oeuvres">Oeuvres</a></span>
   </div> -->
-  <div>
-    {#if oeuvre.description}
-    <Document body={oeuvre.description} />
-    {:else}
-    <p>{oeuvre.bref}</p>
-    {/if}
-  </div>
+  <center class="span__2">
+    <a href="/expositions#" class="button">← Retour à l'exposition</a>
+  </center>
+  <aside>
+    <h1 class="">{oeuvre.titre}</h1>
+    <p>
+      {#if oeuvre.nom}{oeuvre.nom}<br>{/if}
+      {#if oeuvre.ville}{oeuvre.ville}<br>{/if}
+      {#if oeuvre.date}{oeuvre.date}<br>{:else}Non datée<br>{/if}
+    </p>
+    <div>
+      {#if oeuvre.description}
+      <Document body={oeuvre.description} />
+      {:else}
+      <p>{oeuvre.bref}</p>
+      {/if}
+    </div>
+  </aside>
   <figure>
     <Picture media={oeuvre.media} />
   </figure>
@@ -112,7 +119,7 @@
   article {
     color: var(--color);
 
-    > div {
+    aside > div {
       color: var(--dark);
     }
   }
