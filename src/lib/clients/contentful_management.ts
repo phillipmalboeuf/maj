79cabe12888337eaf52856e37aca31da3adb9c8d
Phillip Malboeuf <phil@phils.computer>
2022-10-27 15:55:02 -0400

    createClient

--- src/lib/clients/contentful_management.ts
+++ src/lib/clients/contentful_management.ts
@@ -1,5 +1,4 @@
-import * as pkg from 'contentful-management'
-const { createClient } = pkg
+import { createClient } from 'contentful-management'
 
 export const cma = createClient({
   accessToken: import.meta.env.VITE_CMA_TOKEN as string
